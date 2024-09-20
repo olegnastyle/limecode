20596a9f2bb6c890ea93e8b0f185753eb1fd1411
Nurzhanov Oleg <olegnastyle@yandex.ru>
2024-09-20 17:58:50 +0300

    update: Хлебные крошки

--- client/pages/post/[id].vue
+++ client/pages/post/[id].vue
@@ -1,8 +1,14 @@
 <template>
+    <nav>
+        <ul>
+            <li><nuxt-link to='/'>Главная</nuxt-link></li>
+            <li><strong>{{ post.title }}</strong></li>
+        </ul>
+    </nav>
     <main>
         <div>
             <img src="https://blog.mann-ivanov-ferber.ru/wp-content/uploads/2017/11/untit.jpg" alt="Нет обложки">
-            <h3>{{ post.title }}</h3>
+            <h1>{{ post.title }}</h1>
             <p>{{ post.body }}</p>
             <ul v-for="tag in  post.tags" :key="tag.id">
                 <li>{{ tag }}</li>
@@ -17,9 +23,30 @@
 </script>
 
 <style scoped>
+    nav ul {
+        display: flex;
+        gap: 10px;
+        list-style: none;
+        padding: 0;
+    }
+
+    nav li {
+        display: flex;
+        gap: 10px;
+    }
+
+    nav li:first-child::before {
+        display: none;
+    }
+
+    nav li::before {
+        content: "»";
+    }
+
     img {
         width: 100%;
         height: 240px;
         object-fit: cover;
+        border-radius: 10px;
     }
 </style>
